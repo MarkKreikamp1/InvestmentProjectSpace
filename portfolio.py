9e4d07049636d425059d7f024d2004660470e96f
gregPats <116561383+gregPats@users.noreply.github.com>
2022-10-26 15:03:59 +0200

    Investor Class Added

--- portfolio.py
+++ portfolio.py
@@ -11,6 +11,7 @@ Script for functions on how an investor can interact with stocks. These function
 Script created: Greg Paterson
 """
 from Portfolio.Stocks.stock import Stock
+import wallet
 class Portfolio:
     def __init__(self):
         self.position = {}
@@ -20,39 +21,36 @@ class Portfolio:
         buystock = Stock(tickerSymbol)
         currentprice = buystock.get_current_price()
         quantity = input(f'How much of {buystock} (Current price: {currentprice}) would you like to buy? ')
+        amount = round(float(currentprice) * float(quantity), 2)
         if tickerSymbol not in self.position:
-            self.position[tickerSymbol] = round(float(currentprice) * float(quantity),2)
+            self.position[tickerSymbol] = amount
         else:
-            self.position[tickerSymbol] += round(float(currentprice) * float(quantity), 2)
+            self.position[tickerSymbol] += amount
 
     def Sell(self):
         tickerSymbol = input(f'Which of the following positions: {self.position} would you like to sell? ')
         sellstock = Stock(tickerSymbol)
         currentprice = sellstock.get_current_price()
         quantity = input(f'How much of {sellstock} (Current position: {self.position[tickerSymbol]} would you like to sell? ')
+        amount = round(float(currentprice) * float(quantity), 2)
         if tickerSymbol not in self.position:
             return 'You do not own that stock!'
-        elif float(currentprice) * float(quantity) > self.position[tickerSymbol]:
+        elif amount > self.position[tickerSymbol]:
             return f'Insufficient stock to execute trade!'
         else:
-            self.position[tickerSymbol] -= round(float(currentprice) * float(quantity), 2)
+            self.position[tickerSymbol] -= amount
 
     def __str__(self):
-        a = ''
-        for keys, value in self.position.items():
-            a += f'stock {keys} has balance of {value}\n'
-        return a
+        return str(self.position)
+        # a = ''
+        # for keys, value in self.position.items():
+        #     a += f'stock {keys} has balance of {value}\n'
+        # return a
 
     def __repr__(self):
-        a = ''
-        for keys, value in self.position.items():
-            a += f'stock {keys} has balance of {value}\n'
-        return a
-
-a1 = Portfolio()
-
-a1.Buy()
-a1.Sell()
-
-print(a1)
+        return str(self.position)
+        # a = ''
+        # for keys, value in self.position.items():
+        #     a += f'stock {keys} has balance of {value}\n'
+        # return a
 
